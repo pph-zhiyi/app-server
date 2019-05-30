e4202dc66e48d68f6a4bf3875bd3a22bd7ce0b00
pph-zhiyi <1905225472@qq.com>
2019-05-30 16:56:53 +0800

    add builder

---
+++ src/test/java/com/pph/demo/effective/builder/Test.java
@@ -0,0 +1,23 @@
+package com.pph.demo.effective.builder;
+
+import com.pph.demo.effective.builder.User;
+
+/**
+ * @Author: PPH
+ * @Date: 2019-05-30 15:51
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        User user = new User.Builder(1, "哈哈")
+                .sex(true)
+                .age(20)
+                .height(180.8)
+                .weight(62.5)
+                .addr("杭州")
+                .builder();
+        System.out.println(user.toString());
+    }
+}
---
+++ src/test/java/com/pph/demo/effective/builder/User.java
@@ -0,0 +1,97 @@
+package com.pph.demo.effective.builder;
+
+/**
+ * @Author: PPH
+ * @Date: 2019-05-30 15:52
+ * @Description:
+ */
+public class User {
+
+    private final Integer id;
+
+    private final String name;
+
+    private final Boolean sex;
+
+    private final Integer age;
+
+    private final Double height;
+
+    private final Double weight;
+
+    private final String addr;
+
+    public static class Builder {
+
+        private final Integer id;
+
+        private final String name;
+
+        private Boolean sex;
+
+        private Integer age;
+
+        private Double height;
+
+        private Double weight;
+
+        private String addr;
+
+        public Builder(Integer id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Builder sex(Boolean sex) {
+            this.sex = sex;
+            return this;
+        }
+
+        public Builder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder height(Double height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder weight(Double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder addr(String addr) {
+            this.addr = addr;
+            return this;
+        }
+
+        public User builder() {
+            return new User(this);
+        }
+    }
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.sex = builder.sex;
+        this.age = builder.age;
+        this.height = builder.height;
+        this.weight = builder.weight;
+        this.addr = builder.addr;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", addr='" + addr + '\'' +
+                '}';
+    }
+}

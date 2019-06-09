f16b60f97f47aa133c0e590e8fb65e3f7e7f0d49
pph-zhiyi <1905225472@qq.com>
2019-06-09 12:35:55 +0800

    add lambda

---
+++ src/test/java/com/pph/demo/effective/lambda/L1.java
@@ -0,0 +1,21 @@
+package com.pph.demo.effective.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * @Author: PPH
+ * @Date: 2019-06-09 12:28
+ * @Description:
+ */
+public class L1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.format("Double 类型随机数：%s", doubleRandom()));
+    }
+
+    private static Double doubleRandom() {
+        Supplier<Double> d = () -> Math.random();
+        return d.get();
+    }
+}
